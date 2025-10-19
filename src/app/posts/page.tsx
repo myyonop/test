b9be963b07f978418e599f09000b71c843d7fdfd
myyonop <jeunsu813@gmail.com>
2025-10-20 00:55:50 +0900

    commit

---
+++ src/app/posts/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function PostsPage() {
+    return (
+        <div>
+            <p>User's Page</p>
+            <p>전체 사용자 목록을 보여줍니다</p>
+        </div>
+        
+    )
+}
